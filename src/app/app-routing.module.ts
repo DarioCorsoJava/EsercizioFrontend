3ee44ce32d8f6a6ca0f62156c1c4c8e2659129f0
Dario <dario.c2000@libero.it>
2025-01-17 10:20:46 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/user/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full', component: HomeComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full', component: HomeComponent},
+  {path: 'home', redirectTo: 'home', component: HomeComponent},
+  {path: 'login', redirectTo: 'login', component: LoginComponent},
+  {path: 'registration', redirectTo: 'registration', component: RegistrationComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
